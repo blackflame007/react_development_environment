98ac63ba26c324fe0a8a44cd35c4aa2f44839a1a
Nick <lesseosnick@gmail.com>
2018-03-25 20:37:51 -0700

    React component structure and React router setup with demo

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>About</h1>
+      <p>React, Redux and React Router in Es6 for ultra-responsive web apps.</p>
+      </div>
+    );
+  }
+}
+
+
+export default AboutPage;
